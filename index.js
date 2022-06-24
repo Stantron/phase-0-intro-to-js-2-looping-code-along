f3e9b6960743451de73880d9e8c20ddb72b94949
George Stanton <GLStanton@gmail.com>
2022-06-23 17:38:37 -0700

    commit tree 6e7e6d1bcf53715e5e216a9ee7189acc1aaadd40

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i=0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+
+function writeCards(surprise) {
+    for (let i=0; i < 2; i++) {
+        let surprise = []
+        const name = ["Guadalupe", "Ollie", "Aki"]
+        surprise.push = ('Thank you, ${name[i]}, for the wonderful surprise gift!');
+    return surprise;
+}
+}
+writeCards(surprise);
